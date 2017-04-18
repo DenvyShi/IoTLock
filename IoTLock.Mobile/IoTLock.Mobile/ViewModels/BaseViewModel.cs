96f99fb1be55c55c07161b8fa0d1edd40ef3a04d
Jonathan Braga <jonathanb2br@gmail.com>
2017-04-18 17:56:51 -0300

    Added mobile project

---
+++ IoTLock.Mobile/IoTLock.Mobile/ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace IoTLock.Mobile.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+    }
+}
---
+++ IoTLock.Mobile/IoTLock.Mobile/Views/HomeView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IoTLock.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeView : ContentPage
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+
+        private void SwitchCell_OnChanged(object sender, ToggledEventArgs e)
+        {
+            if(e.Value)
+            {
+                DisplayAlert("Title", "Ativado", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Title", "Desativado", "Ok");
+            }
+            
+        }
+    }
+}
